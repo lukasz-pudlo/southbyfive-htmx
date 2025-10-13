2507d9f90a449e232426a4c40d96f83fbb78da41
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-13 22:49:13 +0100

    Make base url race redirect to list view

--- races/urls.py
+++ races/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.all_races, name='all-races'),
     path('seasons/', views.seasons, name='seasons'),
     path('seasons/<int:season>', views.races, name='races'),
     path('races/', views.all_races, name='all-races'),
