cb1c4cbe6199fff59144b8f43037088a85df4f79
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-16 00:21:42 +0100

    Add stub of upload race file view

--- races/views.py
+++ races/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
-
+from .forms import UploadRaceForm
+from .utils import handle_race_file
 
 from .models import Season, Race, Runner, Result
 
@@ -49,3 +50,14 @@ def race_detail(request, slug):
 @login_required
 def upload_race(request):
     return render(request, 'upload.html')
+
+
+def upload_race_file(request):
+    if request.method == "POST":
+        form = UploadRaceForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_race_file(request.FILES["file"])
+            # return something here, potentially
+    else:
+        form = UploadRaceForm()
+    return render(request, 'upload_partial.html', {"form": form})
