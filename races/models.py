35ad27f9974c5be26b9b9ed2241c6c4a37861a46
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-20 23:02:11 +0100

    Make season model's CharField set-up more readable

--- races/models.py
+++ races/models.py
@@ -6,7 +6,10 @@ from django.core.validators import FileExtensionValidator
 class Season(models.Model):
     # 2526 for 2025/2026
     season = models.CharField(
-        max_length=4, unique=True, help_text="For 2025/2026 season, enter 2526")
+        max_length=4,
+        unique=True,
+        help_text="For 2025/2026 season, enter 2526"
+    )
 
     def __str__(self):
         first_part = self.season[:2]
