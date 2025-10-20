3aa767568ea5e18b307e47cfa7f5d5d634f2c7e6
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-20 23:03:16 +0100

    Create test file for handle_race_file

---
+++ races/tests/test_utils.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.urls import reverse
+from races.models import Season, Race, Runner, Result
+from races.utils import handle_race_file
+from southbyfivehtmx.settings import BASE_DIR
+
+
+class RaceResultListView(TestCase):
+    def setUp(self):
+        filepath = f"{BASE_DIR}/races/tests/resources/kings.xlsx"
+        season = Season.objects.create(
+            season='2526'
+        )
+        handle_race_file(filepath, season)
+
+    def test_get_race_list(self):
+        # Verify the race was created
+        race = Race.objects.filter(slug='kp-2526').first()
+        self.assertIsNotNone(race, "Race with slug 'kp-2526' was not created")
+
+        response = self.client.get(
+            reverse('race-detail', kwargs={'slug': 'kp-2526'}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'race_detail.html')
+        self.assertContains(response, 'kp-2526')
