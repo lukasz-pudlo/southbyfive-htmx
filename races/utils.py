8efe57825e83f49d8af13a27739e35be75b6e885
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-20 21:22:16 +0100

    Remove some debug statements and unused season and race assignments

--- races/utils.py
+++ races/utils.py
@@ -83,24 +83,12 @@ def handle_race_file(file, season):
     df = df.rename(columns={"First Name": "FirstName", "Last Name": "LastName",
                             "Participant Number": "ParticipantNumber"})
     logger.debug(f"The file {file} has the following contents: \n {df}")
-    season = Season.objects.get(pk=season.pk)
-    logger.debug(f"Retrieved the following season from the database: {season}")
-    # Retrieve all races from that season
-    races = Race.objects.filter(season=season)
-    logger.debug(f"Retrieved the following season {season} races: {races}")
-    # Retrieve the race corresponding to the file
     race_name = str(file).split('.')[0]
     logger.debug(f"Variable race_name: {race_name}")
 
     file_runner_rows = []
     for row in df.itertuples():
         file_runner_rows.append(row)
-        # logger.debug(f"Index: {row.Index}, Time: {row.Time}")
-    logger.debug(f"file_runner_rows: {file_runner_rows}")
-    logger.debug(
-        f"One entry from file_runner_rows: {file_runner_rows[0]}")
-    logger.debug(
-        f"One entry from file_runner_rows: {file_runner_rows[0].FirstName}")
 
     # Always delete all races, and results in the current season when uploading.
     # Uploads will always have to start with King's Park.
