74026eed4208a75adbff43be04125a4e1fb998fe
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-10-22 00:13:44 +0100

    Register Classification and ClassificationResult in admin

--- races/admin.py
+++ races/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Race, Runner, Result, Season
+from .models import Race, Runner, Result, Season, Classification, ClassificationResult
 
 
 class SeasonAdmin(admin.ModelAdmin):
@@ -18,7 +18,17 @@ class ResultAdmin(admin.ModelAdmin):
     pass
 
 
+class ClassificationAdmin(admin.ModelAdmin):
+    pass
+
+
+class ClassificationResultAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Season, SeasonAdmin)
 admin.site.register(Race, RaceAdmin)
 admin.site.register(Runner, RunnerAdmin)
 admin.site.register(Result, ResultAdmin)
+admin.site.register(Classification, ClassificationAdmin)
+admin.site.register(ClassificationResult, ClassificationResultAdmin)
